8a73017be01df1f259c53b7b3b9142dc1c35c61e
NewEraCracker <neweracracker@gmail.com>
2011-03-22 19:22:09 +0000

    Added checking another function

--- noobish_host.php
+++ noobish_host.php
@@ -1,57 +1,67 @@
 <?php
-/* iS MA HOS' NOOBISH? v2.4
+/* iS MA HOS' NOOBISH? v2.5.1
  * @author   NewEraCracker
- * @date     22-02-2011
+ * @date     22-03-2011
  * @license  Public Domain
  * @notes    newfags can't triforce
  */
 
+// config
+$functionsToBeDisabled = array('link', 'symlink', 'system', 'shell_exec', 'passthru', 'exec', 'pcntl_exec', 'popen', 'proc_close', 'proc_get_status', 'proc_nice', 'proc_open', 'proc_terminate');
+$functionsToBeEnabled = array('php_uname', 'base64_decode', 'fpassthru', 'ini_set');
+
+// init
 header('Content-Type: text/plain');
 error_reporting(0);
-
-// config
+$crlf = "\r\n";
+$issues = '';
 $noobishPointz = 0;
-$functionsToBeDisabled = array('link', 'symlink', 'system', 'shell_exec', 'passthru', 'exec', 'popen', 'proc_close', 'proc_get_status', 'proc_nice', 'proc_open', 'proc_terminate');
-$functionsToBeEnabled = array('php_uname', 'base64_decode', 'fpassthru');
 
 // functions to be disabled
-foreach ($functionsToBeDisabled as $test) {
-    if (function_exists($test) && !(in_array($test, explode(', ', ini_get('disable_functions'))))) {
+foreach ($functionsToBeDisabled as $test)
+{
+    if (function_exists($test) && !(in_array($test, explode(', ', ini_get('disable_functions')))))
+    {
         $noobishPointz++;
+		$issues .= "Issue: Function ".$test." should be disabled!".$crlf;
     }
 }
 unset($test);
 
 // functions to be enabled
-foreach ($functionsToBeEnabled as $test) {
-    if (!function_exists($test) || in_array($test, explode(', ', ini_get('disable_functions')))) {
+foreach ($functionsToBeEnabled as $test)
+{
+    if (!function_exists($test) || in_array($test, explode(', ', ini_get('disable_functions'))))
+    {
         $noobishPointz++;
+		$issues .= "Issue: Function ".$test." should be enabled!".$crlf;
     }
 }
 unset($test);
 
 // dl (in)security
-if (function_exists('dl') && !(in_array('dl', explode(', ', ini_get('disable_functions'))))) {
-        if (ini_get('enable_dl')) {
+if (function_exists('dl') && !(in_array('dl', explode(', ', ini_get('disable_functions')))))
+{
+        if (ini_get('enable_dl'))
+        {
             $noobishPointz++;
+			$issues .= "Issue: enable_dl should be Off!".$crlf;
         }
 }
 
 // safe_mode?
-if (ini_get('safe_mode')) { $noobishPointz++; }
+if (ini_get('safe_mode')) { $noobishPointz++; $issues .= "Issue: safe_mode is On!".$crlf;}
 
 // magic_quotes_gpc?
-if (ini_get('magic_quotes_gpc')) { $noobishPointz++; }
-
-// ini_alter vs ini_set crap?
-if (function_exists('ini_alter') && (in_array('ini_alter', explode(', ', ini_get('disable_functions'))))) {
-        if (function_exists('ini_set') && !(in_array('ini_set', explode(', ', ini_get('disable_functions'))))) {
-            $noobishPointz++;
-        }
-}
+if (ini_get('magic_quotes_gpc')) { $noobishPointz++; $issues .= "Issue: magic_quotes_gpc is On!"; }
 
 // output results
-if ($noobishPointz==0) { echo('Host is not noobish! Ready for use!' . PHP_EOL); }
-else { echo('Your host scored '.$noobishPointz.', check php.ini if you don\'t mind!'. PHP_EOL); }
-
+if ($noobishPointz==0)
+{
+    echo('Host is not noobish! Ready for use!'.$crlf);
+}
+else
+{
+    echo('Your host scored '.$noobishPointz.' noobish points!'.$crlf.$crlf.$issues); 
+}
 ?>

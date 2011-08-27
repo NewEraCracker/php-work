49576a548f299da548d7bb5a51c661c0db53d8f0
NewEraCracker <neweracracker@gmail.com>
2011-08-27 20:19:21 +0100

    Added more functionality to ipRangeCalculate.php - This will require LOTS of testing

--- ipRangeCalculate.php
+++ ipRangeCalculate.php
@@ -38,7 +38,7 @@ if ( !function_exists('inet_pton'))
 		{
 			return false;
 		}
-		
+
 		if(strlen($ip)==4 || strlen($ip)==16)
 		{
 			return $ip;
@@ -47,7 +47,7 @@ if ( !function_exists('inet_pton'))
 		{
 			return false;
 		}
-		
+
 	}
 }
 
@@ -78,9 +78,9 @@ if ( !function_exists('inet_ntop'))
 				else
 				{
 					if (strpos($res,'::')===false) {
-							if (substr($res,-1)==':') continue;
-							$res.=':';
-							continue;
+						if (substr($res,-1)==':') continue;
+						$res.=':';
+						continue;
 					}
 					$res.=($res==''?'':':').'0';
 				}
@@ -96,22 +96,41 @@ if ( !function_exists('inet_ntop'))
 	}
 }
 
-function ipRangeCalculate($ip,$range)
+/*
+	==================================================
+	Calculates an IP range from an IP and a range code
+	==================================================
+
+	----
+
+	Example:
+
+	To calculate the range of:
+	127.0.0.1/8
+
+	You would pass 127.0.0.1 as the $ip and 8 as the $range
+
+	----
+
+	The function returns an array with the elements
+	'lower' and 'higher' or, in case of failure, false.
+*/
+function ipRangeCalculate( $ip, $range )
 {
 	$ip_address = (string)  @$ip;
 	$ip_range   = (integer) @$range;
 
-	if($ip_address != $ip || $ip_range != $range)
+	if( empty($ip_address) || empty($ip_range) )
 	{
-		// We got wrong types of information
+		// Something is wrong
 		return false;
 	}
 	else
 	{
 		// Validate IP address
 		$ip_address = inet_ntop( inet_pton( $ip_address ) );
-		
-		if(!$ip_address)
+
+		if( !$ip_address )
 		{
 			// Not a good IP
 			return false;
@@ -119,14 +138,14 @@ function ipRangeCalculate($ip,$range)
 	}
 
 	// Pack IP, Set some vars
-	$ip_pack       = inet_pton($ip_address);
+	$ip_pack	   = inet_pton($ip_address);
 	$ip_pack_size  = strlen($ip_pack);
 	$ip_bits_size  = $ip_pack_size*8;
 
 	// IP bits (lots of 0's and 1's)
 	$ip_bits = "";
 	for($i=0; $i < $ip_pack_size; $i=$i+1)
-	{	
+	{
 		$bit = decbin( ord($ip_pack[$i]) );
 		$bit = str_pad($bit, 8, '0', STR_PAD_LEFT);
 		$ip_bits .= $bit;
@@ -154,18 +173,161 @@ function ipRangeCalculate($ip,$range)
 		$chr = chr( bindec($chr) );
 		$ip_higher_pack .= $chr;
 	}
-	
-	return array("lower"=>inet_ntop($ip_lower_pack),"higher"=>inet_ntop($ip_higher_pack));
+
+	return array( 'lower'=>inet_ntop($ip_lower_pack), 'higher'=>inet_ntop($ip_higher_pack));
 }
 
-// Example usage
-//
-// 127.0.0.1/8
-// ipRangeCalculate("127.0.0.1",8);
-//
-if( $range = ipRangeCalculate(@$_GET['ip'],@$_GET['range']) )
+/*
+	=======================================================
+	Calculates if a certain IP is within a certain IP range
+	=======================================================
+
+	It currently supports single IPs (x.x.x.x) and Ranges (x.x.x.x-y.y.y.y and x.x.x.x/n)
+
+	----
+
+	Example:
+
+	$ip = 8.8.8.8
+	$range = 8.8.4.4/16
+
+	This function would return true for that case
+
+	----
+
+	Be aware this function also returns false in case of error, so you better validate the input ;)
+*/
+function isInIpRange( $ip, $range )
 {
-	echo "<pre>";
-	print_r( $range );
-	echo "</pre>";
+	/* -------------------------
+	   Convert IP to packed form
+	   ------------------------- */
+	$ip = inet_pton( $ip );
+
+	if( !$ip )
+	{
+		// This is not right
+		return false;
+	}
+
+	/* --------------------
+	   Calculate the range
+	   -------------------- */
+
+	if( strpos($range,'/') !== false )
+	{
+		// Explode
+		$range = explode( '/', $range );
+
+		// Calculate and validate
+		if( $range = ipRangeCalculate( @$range[0], @$range[1] ) )
+		{
+			$ipRangeLower  = $range['lower'];
+			$ipRangeHigher = $range['higher'];
+		}
+		else
+		{
+			// This is not right
+			return false;
+		}
+	}
+	elseif( strpos($range,'*') !== false )
+	{
+		// Calculate and validate
+		if( strpos($range,'.') !== false )
+		{
+			// IPv4
+			$ipRangeLower = str_replace('*','0',$range);
+			$ipRangeHigher = str_replace('*','255',$range);
+		}
+		elseif( strpos($range,':') !== false )
+		{
+			// IPv6
+			$ipRangeLower = str_replace('*','0',$range);
+			$ipRangeHigher = str_replace('*','ffff',$range);
+		}
+		else
+		{
+			// This is not right
+			return false;
+		}
+	}
+	elseif( strpos($range,'-') !== false )
+	{
+		// Calculate
+		$range		 = explode( '-', $range );
+		$ipRangeLower  = @$range[0];
+		$ipRangeHigher = @$range[1];
+
+		// Validate
+		if( empty($ipRangeLower) || empty($ipRangeHigher) )
+		{
+			// This is not right
+			return false;
+		}
+	}
+	else
+	{
+		// Validate
+		$range = inet_ntop( inet_pton( $range ) );
+
+		if( !$range )
+		{
+			// This is not right
+			return false;
+		}
+
+		// Calculate
+		$ipRangeHigher = $ipRangeLower = $range;
+	}
+
+	/* -------------------------------------------
+	   Convert the calculated range to packed form
+	   ------------------------------------------- */
+
+	$ipRangeHigher = inet_pton( $ipRangeHigher );
+	$ipRangeLower = inet_pton( $ipRangeLower );
+
+	/* --------------
+	   Validate again
+	   -------------- */
+
+	if( strlen($ipRangeHigher) != strlen($ipRangeLower)
+	|| stren($ipRangeLower) != strlen($ip) )
+	{
+		// This is not right
+		return false;
+	}
+
+	/* -------
+	   Compare
+	   ------- */
+
+	// Compare IP to ranges extremes
+	if( $ipRangeHigher = $ip || $ipRangeLower = $ip )
+	{
+		// Thats the IP we are looking for
+		return true;
+	}
+
+	for( $i=0; $i<count($ip); $i++)
+	{
+		if( $ipRangeLower[$i] <= $ip[$i] && $ip[$i] <= $ipRangeHigher )
+		{
+			// This IP bit is into IP Range
+			continue;
+		}
+		else
+		{
+			// We didn't found that IP bit into IP range
+			// So, the IP we are looking for is not this one
+			return false;
+		}
+	}
+
+	/* --------
+	   Found it
+	   -------- */
+
+	return true;
 }

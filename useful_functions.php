98678bda09d2d773d24a270021522a1e75c9a40f
NewEraCracker <neweracracker@gmail.com>
2014-06-27 22:43:01 +0100

    Minor improvements in useful functions

--- useful_functions.php
+++ useful_functions.php
@@ -4,11 +4,13 @@
  License: Public Domain
 */
 
+/** Array with the paths a dir contains */
 function readdir_recursive($dir, $show_dirs=false)
 {
-	return explode("\r\n", readdir_recursive_string($dir, $show_dirs));
+	return explode("\n", readdir_recursive_string($dir, $show_dirs));
 }
 
+/** String with the paths a dir contains */
 function readdir_recursive_string($dir, $show_dirs=false, $dir_len=null)
 {
 	// Be sure about dir
@@ -18,78 +20,78 @@ function readdir_recursive_string($dir, $show_dirs=false, $dir_len=null)
 	if($dir_len == null) $dir_len = strlen($dir);
 
 	// Fun begins
-	ob_start();
+	$ok = ob_start(null);
+	if( !$ok )
+	{
+		trigger_error('Unable to start output buffering', E_USER_WARNING);
+		return '';
+	}
 
 	// Open the dir
 	$dh = opendir($dir);
 
 	// Read the dir
-	while ( ( false !== ( $file = readdir($dh) ) ) && $dh )
+	while( $dh && (false !== ($file = readdir($dh))) )
 	{
 		if( $file != '.' && $file != '..')
 		{
-			$file = realpath($dir."/".$file);
+			$file = realpath($dir.'/'.$file);
 			if( is_dir($file) )
 			{
 				// If $show_dirs is true, dir names will also be listed
 				if( $show_dirs )
-					echo "\r\n".substr($file, $dir_len);
+				{
+					echo "\n".substr($file, $dir_len);
+				}
 
 				// Read recursively another dir below the original dir
 				// We pass $dir_len here so the path is relative to the 'mother' dir
-				echo "\r\n".readdir_recursive_string($file, $show_dirs, $dir_len);
+				echo "\n".readdir_recursive_string($file, $show_dirs, $dir_len);
 			}
 			else
 			{
 				// Just echo the filename
-				echo "\r\n".substr($file, $dir_len);
+				echo "\n".substr($file, $dir_len);
 			}
 		}
 	}
 
-	// Get the result
-	$contents = ob_get_contents();
-
-	// Clean the buffer
-	ob_end_clean();
-
-	// Return our result
-	return ltrim($contents);
+	// Get and return our result
+	return ltrim(ob_get_clean());
 }
 
+/** Normalize a text file by trimming extra whitespace */
 function normalize_text_file($f)
 {
-	// Bail out on error
-	if( !is_file($f) || !is_readable($f) || !is_writable($f) )
-		return;
+	// Check if we can work with the file
+	if( is_file($f) && is_readable($f) && is_writable($f) )
+	{
+		// Grab content
+		$content = file($f);
 
-	// Grab contents and normalize them
-	$c = rtrim(convert_to_crlf(file_get_contents($f)));
+		// Check if content was grabbed
+		if(count($content))
+		{
+			// Detect line ending (Windows CRLF or Unix LF)
+			$eol = ( (strpos($content[0], "\r\n") !== false) ? "\r\n" : "\n" );
 
-	// Explode the normalized contents
-	$c = explode("\r\n",$c);
+			$new = '';
 
-	// Build new contents while normalizing them
-	$new = '';
-	for($i=0;$i<count($c);$i++)
-		$new .= rtrim($c[$i])."\r\n";
-	$new = rtrim($new);
+			// Build new content
+			foreach($content as $line)
+			{
+				$new .= rtrim($line).$eol;
+			}
 
-	// Write the new contents to the file
-	file_put_contents($f,$new);
-}
+			$new = trim($new);
 
-function convert_to_crlf($text)
-{
-	// First, we take care of CRLF itself
-	$text = str_replace("\r\n","\n",$text);
-
-	// Then, we take care of any CR left
-	$text = str_replace("\r","\n",$text);
-
-	// Finally, we convert the LF back to CRLF
-	$text = str_replace("\n","\r\n",$text);
-
-	return $text;
+			// Some files may have intentional whitespace so we'll keep it
+			if(strlen($new) > 0)
+			{
+				// Write the new content
+				file_put_contents($f,$new);
+			}
+		}
+	}
 }
 ?>
